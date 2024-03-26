99868b8f3baccad19d1859a6472c7be491585eb5
dayumstir <dempstertay123@gmail.com>
2024-03-26 17:58:44 +0100

    debug server error

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,16 +3,16 @@ import { createClient } from "~/utils/supabase/server";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const supabase = createClient();
-  const { data, error } = await supabase.auth.getUser();
-  if (error ?? !data?.user) redirect("/login");
+  // const supabase = createClient();
+  // const { data, error } = await supabase.auth.getUser();
+  // if (error ?? !data?.user) redirect("/login");
 
-  const user = await api.user.getById();
-  if (!user) redirect("/getting-started");
+  // const user = await api.user.getById();
+  // if (!user) redirect("/getting-started");
 
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <p>Hey {user.name}!</p>
+      {/* <p>Hey {user.name}!</p> */}
     </div>
   );
 }
