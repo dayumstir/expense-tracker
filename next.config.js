b1d625a695e94d5dbc768fb9bc63cf30368952a5
dayumstir <dempstertay123@gmail.com>
2024-06-08 23:17:03 +0200

    chore: add million lint

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
+import MillionLint from '@million/lint';
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
 import withPWAInit from "@ducanh2912/next-pwa";
-
 const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontendNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   register: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development"
 });
 
 /** @type {import("next").NextConfig} */
 const config = {};
-
-export default withPWA(config);
+export default MillionLint.next({
+  rsc: true
+})(withPWA(config));
